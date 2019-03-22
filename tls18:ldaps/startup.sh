384bc5f93a028cc99d2270be8a231b6b0702b18c
edtasixm06 <asixm06@edt.org>
2019-03-22 21:13:05 +0100

    openssl

--- tls18:ldaps/startup.sh
+++ tls18:ldaps/startup.sh
@@ -4,5 +4,5 @@
 # -------------------------------------
 
 /opt/docker/install.sh && echo "Install Ok"
-/sbin/slapd -d0  && echo "slapd Ok"
+/sbin/slapd -d0 -u ldap -h "ldap:/// ldaps:/// ldapi:///" && echo "slapd Ok"
 
